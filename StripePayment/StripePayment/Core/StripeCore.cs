c0c2216dab7f20c74891414143863021720d2bd4
bjcamposalsiba <jesus.campos@alsiba.mx>
2026-04-29 23:48:48 -0500

    Install Stripe.Net library and defying StripeCore structure

--- StripePayment/StripePayment/Core/StripeCore.cs
+++ StripePayment/StripePayment/Core/StripeCore.cs
@@ -1,8 +1,19 @@
 ﻿
 
+using Stripe;
+
 namespace StripePayment.Core
 {
     public class StripeCore
     {
+        public StripeClient _stripeClientV2 { get; private set; }
+
+        public StripeCore(string apiKey)
+        {
+            if (string.IsNullOrWhiteSpace(apiKey))
+                throw new ArgumentException("API key must be provided", nameof(apiKey));
+
+            _stripeClientV2 = new StripeClient(apiKey);
+        }
     }
 }
